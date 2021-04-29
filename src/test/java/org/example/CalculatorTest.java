69cd6044764b769dcae571dd1cc97246067b357d
Alexander Wiklund <34245732+DoNotWorr@users.noreply.github.com>
2021-04-29 18:56:35 +0200

    Added @DisplayName to tests

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
 
+    @DisplayName("0! should be 1")
     @Test
     public void factorialZero_shouldReturnOne() {
         assertEquals(1, getFactorialOfNumber(0));
     }
 
+    @DisplayName("1! should be 1")
+    @Test
+    public void factorialOne_shouldReturnOne() {
+        assertEquals(1, getFactorialOfNumber(1));
+    }
+
+    @DisplayName("3! should be 6")
+    @Test
+    public void factorialThree_shouldReturnSix() {
+        assertEquals(1, getFactorialOfNumber(1));
+    }
+
+    @DisplayName("(-1)! is undefined and should throw ArithmeticException")
     @Test
     public void factorialNegativeNumber_shouldThrowException() {
         assertThrows(ArithmeticException.class, () -> getFactorialOfNumber(-1));
